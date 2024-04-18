36eb9b8d84371ede5486bc86aa31948c815a77cc
pramod-shanmugam <pramod.sn@webknot.in>
2024-04-18 00:56:02 -0400

    minor fix

--- src/components/book-count.component.js
+++ src/components/book-count.component.js
@@ -1,63 +1,45 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
-export default function BookCount() {
-
-    const [bookCountData, setBookCountData] = useState(-1);
-    const [authorName, setAuthorName] = useState('');
-   
-    const onSubmit = (e) => {
-        e.preventDefault();
-        axios.get('http://localhost:3000/book/reduce/' + authorName)
-            .then((response) => {
-                 setBookCountData(response.data.results[0].value);
-                 setAuthorName(response.data.results[0]._id);
-            })
+export default class BookCount extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { bookCount: {}, error: null };
     }
 
-   
-    return (
-        <div>
-            <h3>Find Book Count for Author</h3>
-        <div >
-        <form onSubmit={onSubmit}>
-          <div className="form-group"> 
-            <label>Author Name: </label>
-            <input  type="text"
-                required
-                className="form-control"
-                value={authorName}
-                onChange={(e) => {setAuthorName(e.target.value)}}
-                />
-          </div>
+    componentDidMount() {
+        axios.get('http://localhost:8080/api/artworks/count-by-artist')
+            .then(response => {
+                this.setState({ bookCount: response.data });
+            })
+            .catch(error => {
+                console.error("Failed to fetch artifacts:", error);
+                this.setState({ error: "Failed to load artifacts." });
+            });
+    }
 
-          <div className="form-group">
-            <input type="submit" value="Search" className="btn btn-primary" />
-          </div>
-
-        </form>
-        </div>
-        <div>
-            {bookCountData >=0 ? 
+    render() {
+        const { bookCount, error } = this.state;
+        return (
             <div>
-            <h3>Result</h3>
-            <table className="table">
-              <thead className="thead-light">
-                <tr>
-                  <th>Author</th>
-                  <th>Book Count</th>                
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                    <td>{authorName}</td>
-                    <td>{bookCountData}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-            : null}
-        </div>
-        </div>
-    )
+                {error && <p className="error">{error}</p>}
+                <table className="table">
+                    <thead className="thead-light">
+                        <tr>
+                            <th>Artist</th>
+                            <th>Count</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {Object.entries(bookCount).map(([artist, count]) => (
+                            <tr key={artist}>
+                                <td>{artist}</td>
+                                <td>{count}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
